5212f67a25290dd20aa086a0b81a1d4666d207c7
mcmckinley <mcmckinley@csuchico.edu>
2024-08-02 00:49:01 -0700

    Major readability improvements on RankedPredictionPage

--- frontend/src/RankedPredictionPage.jsx
+++ frontend/src/RankedPredictionPage.jsx
@@ -31,12 +31,16 @@ const RankedPredictionPage = () => {
 
   const [selectedBoxID, setSelectedBoxID] = useState(null)
 
-  const [map, setMap] = useState(0)
+  const [map, setMap] = useState(null)
   const [previousMap, setPreviousMap] = useState(0)
 
   const [result, setResult] = useState(0.5)
 
   const [isShowingMapSection, setMapSectionVisibility] = useState(false)
+  function toggleMapSectionVisibility () {
+    setMapSectionVisibility(!isShowingMapSection)
+  }
+  
 
   const gameModes      = ['Gem Grab', 'Brawl Ball', 'Knockout', 'Wipeout', 'Heist', 'Hot Zone']
   const gameModeColors = ['#9430C1', '#95B0E4', '#FFBD33', '#33B8DF', '#BF86C6', '#E22525']
@@ -99,39 +103,79 @@ const RankedPredictionPage = () => {
     }
   };
 
+  const getRecommendations = async () => {
+    try {
+      // setIsAwaitingPrediction(true)
+      const blue1 = entries[0] ? parseInt(entries[0], 10) : null;
+      const blue2 = entries[1] ? parseInt(entries[1], 10) : null;
+      const blue3 = entries[2] ? parseInt(entries[2], 10) : null;
+      const red1 = entries[3] ? parseInt(entries[3], 10) : null;
+      const red2 = entries[4] ? parseInt(entries[4], 10) : null;
+      const red3 = entries[5] ? parseInt(entries[5], 10) : null;
+      console.log(blue1)
+
+      const response = await fetch('http://localhost:8000/recommend', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ blue1, blue2, blue3, red1, red2, red3, map }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      // setIsAwaitingPrediction(false)
+      // setPreviousEntries(entries)
+      // setPreviousMap(map)
+      console.log('Got result:', data.prediction)
+
+      // display an error if we can't connect to server
+    } catch (error) {
+      console.error(error)
+      setResult('error');
+    }
+  };
+
   // Runs when the element loads
   useEffect(() => {
-    const shouldGetPredition = !anyEntriesEmpty() && (previousEntries != entries || previousMap != map)
-    if (shouldGetPredition) {
-      console.log('Will get prediciton')
-      console.log('previous entries:', previousEntries)
-      console.log('entries:', entries)
-      getPrediction()
-    } else {
-      console.log('should not get prediction')
-    }
+    // const shouldGetPredition = !anyEntriesEmpty() && (previousEntries != entries || previousMap != map)
+    // if (shouldGetPredition) {
+    //   console.log('Will get prediciton')
+    //   getPrediction()
+    // } else {
+    //   console.log('should not get prediction')
+    // }
+
+    // const shouldGetRecommendations = anyEntriesEmpty() && (previousEntries != entries || previousMap != map)
+    // if (shouldGetRecommendations) {
+    //   console.log('Will get recs')
+    //   getRecommendations()
+    // } else {
+    //   console.log('should not get recommendation')
+    // }
 
     if (teamWithFirstPick == 'Red') {
       setSelectedBoxID(5)
     }
   }, [entries, map, teamWithFirstPick]); // Runs whenever these change
 
-  console.log('BANS:', bans)
-
   const DraftOrderSelector = ({ boxID }) => {
     return (
-        <div className={`thumb-div ${userDraftNumber == boxID ? 'selected-thumb':'unselected-thumb'}`} 
-            onClick={ () => {
-                setUserDraftNumber(boxID)
-                moveToNextSection()
-            }}>
-            <img src={icons["ranked-icon.png"]}   />
-            <p>{draftNumberStrings[boxID]}</p>
-        </div>
+      <div className={`thumb-div ${userDraftNumber == boxID ? 'selected-thumb':'unselected-thumb'}`} 
+          onClick={ () => {
+              setUserDraftNumber(boxID)
+              moveToNextSection()
+          }}>
+          <img src={icons["ranked-icon.png"]}   />
+          <p>{draftNumberStrings[boxID]}</p>
+      </div>
     )
   }
 
-  const MiniBanBoxes = ({ bans }) => {
+  const MiniBanBoxes = () => {
     return ( 
       <div className="mini-ban-boxes">
         {bans.map((ban, index) => (
@@ -152,128 +196,108 @@ const RankedPredictionPage = () => {
     });
   };
 
+  // return the class name for a section.
+  // The class name is determined by the visibility of the section.
+  // The options are 'section-active' and 'section-inactive'
+  function sectionClassName (id) {
+    return `section-${IDofActiveSection == id ? 'active' : 'inactive'}`
+  }
+
+  // The thumbs up buttons to select which team has first pick
+  const SelectWhoHasFirstPick = ({ color }) => {
+    return (
+      <div className={`thumb-div ${teamWithFirstPick == color ? 'selected-thumb':'unselected-thumb'}`} 
+        onClick={ () => {
+          setTeamWithFirstPick(color); 
+          // only push the user forward if its their first time clicking the thumbs up/down
+          if (IDofActiveSection == 0) {
+            moveToNextSection()
+            setMapSectionVisibility(true)
+          }
+        }}>
+        <img src={icons[`thumbs-${color == 'Blue' ? 'up' : 'down'}.png`]}   />
+        <p>{color == 'Blue' ? 'Your' : 'Enemy'} team</p>
+      </div>
+    )
+  }
+
+  // Shown in the top left of a section
+  const SectionTitle = ({ text }) => {
+    return (
+      <div className="section-upper-part-left">
+        <p>{text}</p>
+      </div>
+    )
+  }
+
+  // Shown in the top right of a section.
+  console.log(IDofActiveSection)
+  const SectionDescription = ({ sectionID, text, children }) => {
+    return (
+      <div className="section-upper-part-right">
+        { IDofActiveSection > sectionID && (
+          <p>{text}</p>
+        )}
+        {children}
+      </div>
+    )
+  }
+
+  const Section = ({ id, title, description, hasDoneButton=false, children}) => {
+    console.log(bans)
+    return (
+      <div className={sectionClassName(id)}>
+        <div className='section-upper-part'>
+          <SectionTitle text={title} />
+
+          <SectionDescription sectionID={id} text={description}>
+            {IDofActiveSection == 3 && id == 3 && (
+              <p onClick={moveToNextSection}>Done</p>
+            )}
+            {IDofActiveSection > 3 && id == 3 && (
+              <MiniBanBoxes />
+            )}
+          </SectionDescription>
+        </div>
+        
+        {IDofActiveSection == id && children}
+      </div>
+    )
+  }
+
   return (
     <div className="input-page">
-      <div className='empty-space'></div>
+      {/* <div className='empty-space'></div> */}
+
       {/* Who's picking first? section */}
-      <div ref={firstPickSectionRef} className={`section-${IDofActiveSection == 0 ? 'active' : 'inactive'}`}> 
-          <div className="section-upper-part"> {/* this just works? */}
-            <div className="section-upper-part-left">
-              <p>Which Team Has First Pick?</p>
-            </div>
-            <div className="section-upper-part-right" 
-            onClick={() => {
-              scrollToSection()
-              if (IDofActiveSection == 0) { // Deselect section
-                setIDofActiveSection(1)
-              } else if (IDofActiveSection > 0){
-                setIDofActiveSection(0) // Lets user go back to a previous section.
+      <Section id='0' title='Which team has first pick?' description={teamWithFirstPick} >
+        <div className="first-pick-section">
+          <SelectWhoHasFirstPick color='Blue' />
+          <SelectWhoHasFirstPick color='Red'/>
+        </div>
+      </Section>
+
+      {/* Select Map section */}      
+      <Section id='1' title='Select Map' description={map ? gameModes[maps[map].game_mode] + ' - ' + maps[map].name : ''}>
+        <div className="section-lower-part">
+          <MapSearchBar 
+            selectedMap={map} 
+            setMap={setMap}
+            closeMapSearchBar={()=>setMapSectionVisibility(false)}
+            moveUserForward={() => {
+              if (IDofActiveSection == 1) {
+                setMapSectionVisibility(false)
+                setIDofActiveSection(2)
               }
-            }}>
-            {IDofActiveSection == 0 ? (
-              <>
-                <FaChevronDown />
-                <p>Done</p>
-              </>
-            ) : (
-              <>
-                <FaChevronUp />
-                <p className={teamWithFirstPick}>{teamWithFirstPick}</p>
-              </>
-            )}
-            
-          </div>
-          </div>
-          {(IDofActiveSection == 0 &&
-            <div className="first-pick-section">
-              <div className={`thumb-div ${teamWithFirstPick == 'Blue' ? 'selected-thumb':'unselected-thumb'}`} 
-                onClick={ () => {
-                  setTeamWithFirstPick('Blue'); 
-                  // only push the user forward if its their first time clicking the thumbs up/down
-                  if (IDofActiveSection == 0) {
-                    moveToNextSection()
-                    setMapSectionVisibility(true)
-                  }
-                }}>
-                <img src={icons["thumbs-up.png"]}   />
-                <p>Your team</p>
-              </div>
-              <div className={`thumb-div ${teamWithFirstPick == 'Red' ? 'selected-thumb':'unselected-thumb'}`} 
-                onClick={ () => {
-                  setTeamWithFirstPick('Red'); 
-                  if (IDofActiveSection == 0) {
-                    moveToNextSection()
-                    setMapSectionVisibility(true)
-                  }
-                }}>
-                <img src={icons["thumbs-down.png"]}   />
-                <p>Enemy team</p>
-              </div>
-            </div>
-          )}
-      </div> 
+            }}
+          />
+        </div>
+      </Section>
 
-      {/* Map section */}
-      <div className={`section-${IDofActiveSection == 1 ? 'active' : 'inactive'}`}>
-          <div className="section-upper-part">
-          <div className="section-upper-part-left"> {/* this just works? */}
-              <p>Select Map</p>
-          </div>
-            <div
-                className="section-upper-part-right" 
-                onClick={() => setMapSectionVisibility(!isShowingMapSection)}>
-                {isShowingMapSection ? (
-                    <FaChevronUp color="#bbb"/>
-                ) : (
-                    <FaChevronDown color="#bbb"/>
-                )}
-                <p className="unselectable">{gameModes[maps[map].game_mode] + ' - ' + maps[map].name}</p>
-            </div>
-          </div>
-
-          { isShowingMapSection && (
-          <div className="section-lower-part">
-              <MapSearchBar 
-              selectedMap={map} 
-              setMap={setMap}
-              closeMapSearchBar={()=>setMapSectionVisibility(false)}
-              moveUserForward={() => {
-                if (IDofActiveSection == 1) {
-                  setMapSectionVisibility(false)
-                  setIDofActiveSection(2)
-                }
-              }}
-              />
-          </div>
-          )}
-
-      </div>
 
       {/* When Do You Pick? section */}
-      <div className={`section-${IDofActiveSection == 2 ? 'active' : 'inactive'}`}>
-        <div className="section-upper-part">
-          <div className="section-upper-part-left">
-            <p>When Do You Pick?</p>
-          </div>
-          <div className="section-upper-part-right" 
-            onClick={() => {
-              if (IDofActiveSection == 2) { // Deselect section
-                setIDofActiveSection(3)
-              } else if (IDofActiveSection > 2){
-                setIDofActiveSection(2) // Lets user go back to a previous section.
-              }
-            }}>
-            {IDofActiveSection == 2 ? (
-              <FaChevronUp color="#bbb"/>
-            ) : (
-              <FaChevronDown color="#bbb"/>
-            )}
-            <p>{draftNumberStrings[userDraftNumber]}</p>
-          </div>
-        </div>
-
-        {(IDofActiveSection == 2 &&
-          <div className="first-pick-section">
+      <Section id='2' title='When do you pick?' description={draftNumberStrings[userDraftNumber]}>
+        <div className="first-pick-section">
           {(teamWithFirstPick == 'Blue' && 
             <>
               <DraftOrderSelector boxID={0} />
@@ -289,69 +313,38 @@ const RankedPredictionPage = () => {
             </>
           )}
         </div>
-        )}
-      </div> 
+      </Section>
 
       {/* Enter Bans section */}
-      <div className={`section-${IDofActiveSection == 3 ? 'active' : 'inactive'}`}>
-        <div className="section-upper-part">
-          <div className="section-upper-part-left">
-            <p>Enter Bans</p>
-          </div>
-          
-          <div className="section-upper-part-right" onClick={() => {
-              if (IDofActiveSection == 3) { // Deselect section
-                setIDofActiveSection(4)
-              } else if (IDofActiveSection > 3){
-                setIDofActiveSection(3) // Lets user go back to a previous section.
-              }
-            }}>
-            {IDofActiveSection != 3 ? (
-              <>
-                <FaChevronDown />
-                <MiniBanBoxes bans={bans} />
-              </>
-            ) : (
-              <p>Done</p>
-            )}
-          </div>
-        </div>
-        
-        {IDofActiveSection == 3 && (
-          <>
-            {(bans.length > 0 && 
-              <div className="teams">
-                {
-                  bans.map((ban, index) => (
-                    <div className="ban-box" onClick={()=>removeBan(index)}>
-                      <img key={index} src={icons[brawlers[ban].imgUrl]} />
-                      <FaTimes className='x-button'/>
-                    </div>
-                  ))
-                }
-              </div>
-          )}
 
-            <div className="section-lower-part">  
-              <BrawlerGallery 
-                  banMode={true}
-                  selectedBoxID={null} 
-                  setSelectedBoxID={null} 
-                  entries={bans} 
-                  setEntries={setBans}
-                  closeBrawlerSection={()=>{}}
-              />
+      <Section id='3' title='Enter bans' description=''>
+        <div className="teams">
+          {bans.map((ban, index) => (
+            <div key={index} className="ban-box" onClick={()=>removeBan(index)}>
+              <img src={icons[brawlers[ban].imgUrl]} />
+              <FaTimes className='x-button'/>
             </div>
-          </>
-        )}
-      </div> 
+          ))}
+        </div>
+
+        <div className="section-lower-part">  
+          <BrawlerGallery 
+              banMode={true}
+              selectedBoxID={null} 
+              setSelectedBoxID={null} 
+              entries={bans} 
+              setEntries={setBans}
+              closeBrawlerSection={()=>{}}
+          />
+        </div>
+      </Section>
+
+      
 
       {/* Select Brawler section */}
-      <div className="section">
+      <div className={sectionClassName(4)}>
           <div className="section-upper-part">
-            <div className="section-upper-part-left">
-                <p>Select Brawlers</p>
-            </div>
+            <SectionTitle text='Select Brawlers' />
             <div className="section-upper-part-right">
               <p className="selected">{entryGuides[rankedModeSelectionIndex]}</p>
             </div>
@@ -418,11 +411,8 @@ const RankedPredictionPage = () => {
       {/* Prediciton Description */}
       <div className="section">
           <div className="section-upper-part">
-          <div className="section-upper-part-left">
-              <p>Prediction</p>
-          </div>
-          <div className="section-upper-part-right">
-          </div>
+          <SectionTitle text='Prediction' />
+         
           </div>
 
           <div className="section-lower-part">
