d8d752a7da92a926771f60813cb675fbf4ba2fa2
mcmckinley <mcmckinley@csuchico.edu>
2024-08-24 16:13:53 -0700

    Page no longer unecessarily update when sidebar is hidden/shown

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,52 +2,22 @@
 import React, { useState, useEffect } from 'react';
 import PredictOutcomePage from './PredictOutcomePage';
 import RankedPredictionPage from './RankedPredictionPage';
+import Sidebar from './Sidebar';
 
 const App = () => {
-  // UI states
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
 
   const [page, setPage] = useState(true)
   const togglePage = () => {
     setPage(!page)
   }
 
-  // Called when size of page changes
-  const handleResize = () => {
-    if (window.innerWidth <= 768) {
-      setIsMobile(true);
-      setIsSidebarOpen(false);
-    } else {
-      setIsMobile(false);
-      setIsSidebarOpen(true);
-    }
-  };
-
-  // Toggles sidebar visibility
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
-
-
-  // Runs when the element loads
-  useEffect(() => {
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize); // cleanup function
-  }, []); // Runs whenever these change
-
   return (
     <div className="main">
-      <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
-        <h1>BrawlMind</h1>
-        <p className= {`sidebar-page-selector ${page ? '' : 'active'}`} onClick={() => setPage(false)}>Match Predictor</p>
-        <p className= {`sidebar-page-selector ${page ? 'active' : ''}`} onClick={() => setPage(true)}>Ranked Predictor</p>
-      </div>
+      <Sidebar />
       {page ? (
-        <RankedPredictionPage isSidebarOpen={isSidebarOpen}/>
+        <RankedPredictionPage />
       ) : (
-        <PredictOutcomePage isSidebarOpen={isSidebarOpen}/>
+        <PredictOutcomePage />
       )}
     </div> // main
   );
---
+++ frontend/src/Sidebar.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react'
+
+const Sidebar = ({ page }) => {
+    // UI states
+    const [isSidebarOpen, setIsSidebarOpen] = useState(window.innerWidth > 791);
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 791);
+
+    // Called when size of page changes
+    const handleResize = () => {
+    if (window.innerWidth <= 791) {
+        setIsMobile(true);
+        setIsSidebarOpen(false);
+    } else {
+        setIsMobile(false);
+        setIsSidebarOpen(true);
+    }
+    };
+
+    // Toggles sidebar visibility
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
+
+    // Runs when the element loads
+    useEffect(() => {
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize); // cleanup function
+    }, []); // Runs whenever these change
+
+    return (
+        <>
+            <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+                <h1>BrawlMind</h1>
+                <p className= {`sidebar-page-selector ${page ? '' : 'active'}`} onClick={() => setPage(false)}>Match Predictor</p>
+                <p className= {`sidebar-page-selector ${page ? 'active' : ''}`} onClick={() => setPage(true)}>Ranked Predictor</p>
+            </div>
+            <div className={isSidebarOpen ? 'empty-space-for-sidebar' : ''}></div>
+        </>
+    )
+}
+
+export default Sidebar
