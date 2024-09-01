9c884490bf2b15de48275a1051fe9259d52d28b9
mcmckinley <mcmckinley@csuchico.edu>
2024-09-01 13:02:44 -0700

    Creates About page

---
+++ frontend/src/AboutPage.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import symbols from './symbolLoader';
+
+const AboutPage = ({ setPageIndex }) => {
+    const NavigationButton = ({ text, destinationIndex }) => {
+        return <div className='navigation-button' 
+            onClick={() => {
+                setPageIndex(destinationIndex)
+            }}> 
+            <p>{text}</p>
+        </div>
+    }
+
+    return (
+        <div className='about-page'>
+            <div className='about-page-item about-page-header'>
+                <NavigationButton text={'Home'} destinationIndex={0} />
+                <NavigationButton text={'Engine'} destinationIndex={1} />
+            </div>
+            
+            <h1 className='about-page-item'>About</h1>
+            
+            <div className='icon-row about-page-item'>
+                <img src={symbols['github-mark.svg']}></img>
+                <img src={symbols['reddit.svg']}></img>
+            </div>
+
+            <h2 className='about-page-item'>Created by Michael McKinley</h2>
+        </div>
+    )
+}
+
+export default AboutPage
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,21 +3,17 @@ import React, { useState, useEffect } from 'react';
 import HomePage from './HomePage';
 import PredictOutcomePage from './PredictOutcomePage';
 import RankedPredictionPage from './RankedPredictionPage';
+import AboutPage from './AboutPage'
 import Sidebar from './Sidebar';
 
 const App = () => {
 
-  const [pageIndex, setPageIndex] = useState(0)
+  const [pageIndex, setPageIndex] = useState(2)
 
   var pages = [
     <HomePage setPageIndex={setPageIndex}/>,
     <RankedPredictionPage setPageIndex={setPageIndex} />, 
-    // <PredictOutcomePage />
-  ]
-  var pageNames = [
-    'Home', 
-    'Ranked Predictor',
-    // '3v3 Predictor'
+    <AboutPage setPageIndex={setPageIndex}/>
   ]
 
   return pages[pageIndex]
