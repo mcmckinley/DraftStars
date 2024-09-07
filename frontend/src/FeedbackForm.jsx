bdad22070a08a8ad1c99291b0e90f2546f0efefb
Michael McKinley <mcmckinley@csuchico.edu>
2024-09-06 21:29:28 -0700

    Switches to using draftstars.net domain

--- frontend/src/FeedbackForm.jsx
+++ frontend/src/FeedbackForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 const FeedbackForm = () => {
     return (
-        <form id="feedbackForm" className='feedback-form' method="POST" action="http://localhost:8000/submit-feedback">
+        <form id="feedbackForm" className='feedback-form' method="POST" action="http://draftstars.net/api/submit-feedback">
             {/* <label for="name">Name:</label>
             <input type="text" id="name" name="name" placeholder="" required /> */}
             
